638214de8d8fc74dec26c54f751a91964d2b01a1
Miguel Alejandro Salgado Zapien <mikesalzap@gmail.com>
2019-06-09 01:54:15 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+"""setup.py
+"""
+from setuptools import setup
+
+def load_from_file(filename):
+    with open(filename) as f:
+        returnable = f.read()
+    return returnable
+
+setup(
+    name = "jinjasimplecli",
+    version = "0.0.1",
+    author = "Miguel Salgado",
+    author_email = "projects@ekiim.xyz",
+    description = ("Jinja Simple CLI, is a tool that allows you to compose"
+                   " and insert data in to templates, with support for "
+                   "template inheritance and global functions. It uses "
+                   "json files as input for the data, and it can handle "
+                   "templates from standard input."),
+    long_description=load_from_file('README'),
+    license = "BSD",
+    keywords = "jinja templates cli",
+    url = "https://github.com/ekiim/jinjasimplecli",
+    packages=['an_example_pypi_project', 'tests'],
+    entry_points={
+        'console_scripts': [
+            'jinja-cli = jinjasimplecli.cli:main',
+        ]
+    }
+)
+
